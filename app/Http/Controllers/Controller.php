d0f6c18b96207ea7922b38f443f242844114bcdc
acallejp97 <acallejp@gmail.com>
2019-02-07 12:39:45 +0100

    merge

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,29 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Http\Request;
 use App\AlumnoModel;
-use Illuminate\Support\Facades\Input;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 
-    public function inicio(){
-        $texto = "sdfhuivhfdsulivbhufdshviñofdshvñisdfjiovhsdfiñohviñofsn";
-        
-        return view('welcome')->with('texto',$texto);
+    public function inicio()
+    {
+
+
+        return view('welcome')->with('usuarios', $texto);
     }
 
-    public function aniadirUsuario(Request $request){
+    public function aniadirUsuario(Request $request)
+    {
         // Cogemos el nombre y lo añadimos a la base de datos
         $nombre = $_POST['nombre'];
         $nuevoUsuario = new AlumnoModel;
-        $nuevoUsuario->insert(['nombre'=>$nombre,'id_profesor'=>rand(1,3)]);
+        $nuevoUsuario->insert(['nombre' => $nombre, 'id_profesor' => rand(1, 3)]);
         return redirect('/'); //Esto lo hacemos para que no se nos quede en una pantalla en blanco
     }
 
